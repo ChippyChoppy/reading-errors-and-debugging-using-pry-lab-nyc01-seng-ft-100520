b19af63b94df3cf827605b3806a86799eb3a0502
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-09-22 04:18:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    do 10.times  "s" + string
+  end 
   else
     string
+      binding.pry
   end
 end
+
+snake_it_up("surprise!")
